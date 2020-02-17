b35092c134d95abd70b01e6678c7d6f57717bedc
Jakub Kraus <poczta.jakub1@gmail.com>
2020-02-17 16:39:24 +0100

    Initial set-up

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          A future webpage using <code>React.js</code> as the driving force
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/emiliaguzik/SP"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
