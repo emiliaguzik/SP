9422aa8f5b38fa9ce196f251cb021c09b741c12a
Jakub Kraus <poczta.jakub1@gmail.com>
2020-02-18 22:45:48 +0100

    Added the contact page

---
+++ src/Contact.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <div className="App">
+      <h1>Contact</h1>
+      <p>
+        This is the <span className="hl">contact</span> page.
+      </p>
+    </div>
+  );
+}
+
+export default Contact;
