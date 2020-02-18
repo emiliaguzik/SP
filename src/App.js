544d78801e73d34204d85809c3a3f0055fd6240b
Jakub Kraus <poczta.jakub1@gmail.com>
2020-02-18 22:33:52 +0100

    Added animations for page changing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 import "./App.css";
 import Home from "./Home";
 import About from "./About";
@@ -33,12 +34,20 @@ function App() {
           </li>
         </ul>
       </nav>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/about" component={About} />
-        <Route exact path="/projects" component={Projects} />
-        <Route exact path="/blog" component={Blog} />
-      </Switch>
+      <Route
+        render={({ location }) => (
+          <TransitionGroup>
+            <CSSTransition key={location.key} timeout={450} classNames="fade">
+              <Switch location={location}>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/about" component={About} />
+                <Route exact path="/projects" component={Projects} />
+                <Route exact path="/blog" component={Blog} />
+              </Switch>
+            </CSSTransition>
+          </TransitionGroup>
+        )}
+      />
     </Router>
   );
 }
