e611ac9932aeed2047bff68f9b76a9c5820fe27b
Jakub Kraus <poczta.jakub1@gmail.com>
2020-02-20 22:19:13 +0100

    Updates

---
+++ precache-manifest.1a77077a535ea8c945dea245b1f7407c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60a315fdd79bfea91e66506b2e33c4f6",
+    "url": "/SP/index.html"
+  },
+  {
+    "revision": "7834a785afe3dbbe8b9b",
+    "url": "/SP/static/css/main.530e766e.chunk.css"
+  },
+  {
+    "revision": "46d5c85cdc853066393f",
+    "url": "/SP/static/js/2.09f8f210.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/SP/static/js/2.09f8f210.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7834a785afe3dbbe8b9b",
+    "url": "/SP/static/js/main.bd95d5e5.chunk.js"
+  },
+  {
+    "revision": "ea400bcb4d988e832312",
+    "url": "/SP/static/js/runtime-main.5156aef8.js"
+  },
+  {
+    "revision": "88d1d2ab9e66bca16b48100a4470d37a",
+    "url": "/SP/static/media/logo.88d1d2ab.svg"
+  }
+]);
